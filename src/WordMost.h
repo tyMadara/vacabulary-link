e35c8a1a2b241c40322e1486b2e605ce367644b1
2390363806 <2390363806@qq.com>
2019-03-23 20:43:02 +0800

    同步

--- src/WordMost.h
+++ src/WordMost.h
@@ -2,6 +2,8 @@
 #define WORDMOST_H
 #include<iostream>
 #include<vector>
+#include<time.h>
+#include<stdlib.h>
 #include"WordGraph.h"
 
 class ArcNode;
@@ -18,15 +20,19 @@ class WordMost
     public:
         WordMost(WordGraph &g):graph(g) {}
         virtual ~WordMost();
-        void linkSearch();
+        void wordMostSearch();
+        void letterMostSearch();
         void setMaxLength(int len){maxlength = len;}
         int getMaxlength() {return maxlength;}
         void setQlist(std::vector<ArcNode> &qlist) {maxQlist.clear(); maxQlist.assign(qlist.begin(), qlist.end());}
+		void wordMostLargeScaleSearch();
+		void setTimeLimit(time_t time) { timelimit = time; }
         std::vector<ArcNode> maxQlist;
 
     protected:
 
     private:
+		time_t timelimit = 1000;
 		WordGraph &graph;
         int maxlength = 0;
 
