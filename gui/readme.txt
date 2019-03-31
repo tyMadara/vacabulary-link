c198cf8b8eb2b1a9a22659145264ad841be30cb1
yxyyyxxyy <yixinyu@mail.ustc.edu.cn>
2019-03-31 20:48:17 +0800

    init

--- gui/readme.txt
+++
@@ -1,3 +0,0 @@
-文件夹说明：
-	src				源码
-	vocabularyLink_release0.2		可执行图形界面文件
--- gui/src/getresult.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_RESULT_H
-#define GET_RESULT_H
-
-#include <vector>
-#include <string>
-
-std::string getresult(std::vector <std::string> &words, bool w, bool c, char h, char t, int n);
-
-#endif
